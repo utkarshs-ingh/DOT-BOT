059cd81d235547733c46b3201826d5b34c3e4d25
utkarss_ingh <utkarshsingh.1306@gmail.com>
2021-04-19 23:00:40 +0530

    port bounded

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 import os
+import socket
 
 DEBUG = os.getenv("DEBUG", False)
+sock = socket.socket() 
+port = int(os.environ.get('PORT', 17995)) or 3000
 
 if DEBUG:
     print("Debug Mode")
@@ -15,4 +18,5 @@ else:
     from dotenv import load_dotenv
     env_path = Path(".") / ".env"
     load_dotenv(dotenv_path = env_path)
+    sock.bind(('', port))     
     from settings_files.production import *
