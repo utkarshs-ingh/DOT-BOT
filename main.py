66d80db7e05e49492bb565a796d1d2eb0926a617
utkarss_ingh <utkarshsingh.1306@gmail.com>
2021-04-19 23:56:59 +0530

    server added

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from discord.ext import commands
 from settings import *
 import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # self.send_response(200)
+        # self.send_header("Content-type", "text/html")
+        # self.end_headers()
+        # self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        # self.wfile.write(bytes("<body>", "utf-8"))
+        # self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        # self.wfile.write(bytes("</body></html>", "utf-8"))
+        bot = commands.Bot(command_prefix="!")
+
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py") and filename != "__init__.py":
+                bot.load_extension(f'cogs.{filename[:-3]}')
+        
+        bot.run(DISCORD_API_KEY)
 
 
-bot = commands.Bot(command_prefix="!")
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py") and filename != "__init__.py":
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-
-bot.run(DISCORD_API_KEY)
+server_object = HTTPServer(('', os.getenv("PORT", 3000)), MyServer)
+server_object.serve_forever()
--- settings.py
+++ settings.py
@@ -1,9 +1,6 @@
 import os
-import socket
 
 DEBUG = os.getenv("DEBUG", False)
-sock = socket.socket() 
-port = int(os.environ.get('PORT', 17995)) or 3000
 
 if DEBUG:
     print("Debug Mode")
@@ -17,6 +14,5 @@ else:
     from pathlib import Path
     from dotenv import load_dotenv
     env_path = Path(".") / ".env"
-    load_dotenv(dotenv_path = env_path)
-    sock.bind(('', port))     
+    load_dotenv(dotenv_path = env_path)  
     from settings_files.production import *
