ebcc71846e9c4c5018d1d1b8a1a94d62085f6e79
utkarss_ingh <utkarshsingh.1306@gmail.com>
2021-04-20 09:17:23 +0530

    server added

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ from settings import *
 import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # self.send_response(200)
-        # self.send_header("Content-type", "text/html")
-        # self.end_headers()
-        # self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        # self.wfile.write(bytes("<body>", "utf-8"))
-        # self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        # self.wfile.write(bytes("</body></html>", "utf-8"))
-        bot = commands.Bot(command_prefix="!")
+# class MyServer(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header("Content-type", "text/html")
+#         self.end_headers()
+#         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+#         self.wfile.write(bytes("<body>", "utf-8"))
+#         self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+#         self.wfile.write(bytes("</body></html>", "utf-8"))
+#         bot = commands.Bot(command_prefix="!")
 
-        for filename in os.listdir("./cogs"):
-            if filename.endswith(".py") and filename != "__init__.py":
-                bot.load_extension(f'cogs.{filename[:-3]}')
+#         for filename in os.listdir("./cogs"):
+#             if filename.endswith(".py") and filename != "__init__.py":
+#                 bot.load_extension(f'cogs.{filename[:-3]}')
         
-        bot.run(DISCORD_API_KEY)
+#         bot.run(DISCORD_API_KEY)
 
 
 
-server_object = HTTPServer(('', int(os.getenv("PORT", 3000))), MyServer)
-server_object.serve_forever()
+bot = commands.Bot(command_prefix="!")
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(DISCORD_API_KEY)
